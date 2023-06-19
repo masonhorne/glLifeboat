5c42041c982f502650fac9de7987fe5eefcc2679
masonhorne <masonhorne1@gmail.com>
2023-06-19 18:48:47 -0400

    create lifeboat rendering object

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # glLifeboat
 
-A javascript library for rendering to the WebGL Canvas
+A javascript library for rendering to the WebGL Canvas.
+
+## Contributing
+
+If you would like to contribute to the project please provide documentation for your code in the [jsdoc](https://jsdoc.app) format.
---
+++ glLifeboat/src/lifeboat.js
@@ -0,0 +1,59 @@
+'use strict';
+import { resetForRender } from "./utility/webglutils.js";
+import { sleep } from "./utility/sleep.js";
+
+/**
+ * This class contains a lifeboat for managing canvas renders
+ */
+export class Lifeboat {
+
+    /** Active elements in the lifeboat */
+    activeElements = [];
+    
+    /** Frames per second to render in dynamic mode */
+    FPS = 60;
+
+    /**
+     * @typedef {number} RenderStyle
+     */
+
+    /** 
+     * Render styles available for the lifeboat
+     * @enum {RenderStyle} style to render the canvas
+     */
+    static RENDER_STYLE = {
+        STATIC: 0,
+        DYNAMIC: 1,
+    };
+
+    /** 
+     * Construct a lifeboat for the requested canvas
+     * @param {string} canvasid id of the canvas to render to
+     * @param {RenderStyle} [renderStyle] style to render the canvas in
+     */
+    constructor(canvasId, renderStyle = 0) {
+        const ctx = document.getElementById(canvasId);
+        this.gl = ctx.getContext("webgl");
+        this.renderStyle = renderStyle;
+    }
+
+    /**
+     * Adds a shape into the lifeboat context
+     * @param {Shape} shape shape to add to the canvas
+     */
+    addShape(shape) {
+        this.activeElements.push(shape);
+    }
+
+    /**
+     * Starts rendering lifeboat context to the canvas
+     */
+    async float() {
+        do {
+            resetForRender(this.gl);
+            this.activeElements.forEach((el) => el.render(this.gl));
+            this.activeElements.forEach((el) => el.update());
+            await sleep(1 / this.FPS);
+        } while(this.renderStyle);
+    }
+}
