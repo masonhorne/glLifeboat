eda4559317454118388061b42e4f09f40df7c735
masonhorne <masonhorne1@gmail.com>
2023-06-19 16:42:48 -0400

    setup project to build api docs with github actions

--- glLifeboat/src/utility/webglutils.js
+++ glLifeboat/src/utility/webglutils.js
@@ -1,5 +1,10 @@
 'use strict';
 
+/**
+ * Resizes the canvas to its display size if needed
+ * @param {WebGLRenderingContext} canvas rendering context for the canvas
+ * @returns true if the canvas needed to be resized and false otherwise
+ */
 export function resizeCanvasToDisplaySize(canvas) {
     const devicePixelRatio = window.devicePixelRatio;
     const displayWidth = Math.round(canvas.clientWidth * devicePixelRatio);
@@ -12,6 +17,10 @@ export function resizeCanvasToDisplaySize(canvas) {
     return needResize;
 }
 
+/**
+ * Resets the canvas to render another frame
+ * @param {WebGLRenderingContext} gl rendering context for the canvas
+ */
 export function resetForRender(gl) {
     resizeCanvasToDisplaySize(gl.canvas);
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
@@ -19,6 +28,13 @@ export function resetForRender(gl) {
     gl.clear(gl.COLOR_BUFFER_BIT);
 }
 
+/**
+ * Creates a shader program of the given type with the provided source code
+ * @param {WebGLRenderingContext} gl rendering context for canvas
+ * @param {number} type enum value of shader type
+ * @param {string} source shader source code
+ * @returns shader object if successful otherwise undefined
+ */
 export function createShader(gl, type, source) {
     const shader = gl.createShader(type);
     gl.shaderSource(shader, source);
@@ -32,6 +48,13 @@ export function createShader(gl, type, source) {
     }
 }
 
+/**
+ * Creates a program with the two provided shaders
+ * @param {WebGLRenderingContext} gl rendering context for the canvas 
+ * @param {WebGLShader} vertexShader vertex shader for the program
+ * @param {WebGLShader} fragmentShader fragment shader for the program
+ * @returns program object if successful otherwise undefined
+ */
 export function createProgram(gl, vertexShader, fragmentShader) {
     const program = gl.createProgram();
     gl.attachShader(program, vertexShader);
