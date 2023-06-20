0760f438cfbfc3d91701855919a55f1a0cb9cb8b
masonhorne <masonhorne1@gmail.com>
2023-06-19 20:09:15 -0400

    create npm package deployment configuration

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import { Shape } from "./shapes/shape.js";
+import { Rectangle } from "./shapes/rectangle.js";
+import { Triangle } from "./shapes/triangle.js";
+import { Blue, Color, Green, Red } from "./utility/color.js";
+import { Point } from "./utility/point.js";
+import { Lifeboat } from "./lifeboat/lifeboat.js";
+
+export { 
+    Shape,
+    Rectangle,
+    Triangle,
+    Color,
+    Point,
+    Lifeboat,
+    Blue,
+    Red,
+    Green
+};
