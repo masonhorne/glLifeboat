139a659599fe8d3133672331df5d6324b3ce2d53
Mason Horne <71307091+masonhorne@users.noreply.github.com>
2023-07-01 02:42:35 -0400

    update shaders to render shapes in 3d

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # glLifeboat
+[![NPM version](https://badge.fury.io/js/gllifeboat.svg)](http://badge.fury.io/js/gllifeboat)
+[![npm](https://img.shields.io/npm/dm/gllifeboat.svg?maxAge=2592000)](https://npmcharts.com/compare/gllifeboat?minimal=true)
 
 A javascript library for rendering to the WebGL Canvas.
+
+Install the latest version of glLifeboat from [npm](https://www.npmjs.com/package/gllifeboat).
+
+```bash
+npm install gllifeboat
+```
 
 ## Contributing
 
--- src/object/shape/shape.js
+++ src/object/shape/shape.js
@@ -1,15 +1,68 @@
 'use strict';
 import { Renderable } from "../model/renderable.js";
 import { throwAbstractClassError } from "../../utility/logging.js";
+import { degreeToRadian } from "../../utility/math.js";
+import { multiply, rotationX, rotationY, rotationZ, translation } from "../../utility/math.js";
 
 /**
  * This is an abtract class containing shape functionality
  */
 export class Shape extends Renderable {
+
+    /** These values are the rotation of the shape in radian */
+    xRotation = 0;
+    yRotation = 0;
+    zRotation = 0;
+    /** These are the min x and y values for the shape */
+    minX = undefined;
+    minY = undefined;
+    minZ = undefined;
+
     constructor() { 
         super();
         if(this.constructor.name == Shape) {
             throwAbstractClassError(this.constructor.name);
         }
+    }
+
+    /**
+     * Calculates and returns the shape current rotation matrix
+     */
+    calculateRotation() {
+        return multiply(rotationX(this.xRotation), multiply(rotationY(this.yRotation), rotationZ(this.zRotation)));
+    }
+
+    /**
+     * Calculates and returns the current renderables offset matrix from origin
+     */
+    calculateOffset() { 
+        return translation(this.minX === undefined ? 0 : this.minX,
+            this.minY === undefined ? 0 : this.minY,
+            this.minZ === undefined ? 0 : this.minZ
+        );
+    }
+
+    /**
+     * Sets the rotation value for this shape on the x axis
+     * @param {number} angleInDegree degree to rotate the shape by on the x axis
+     */
+    setXRotation(angleInDegree) {
+        this.xRotation = degreeToRadian(angleInDegree);
+    }
+
+    /**
+     * Sets the rotation value for this shape on the y axis
+     * @param {number} angleInDegree degree to rotate the shape by on the y axis
+     */
+    setYRotation(angleInDegree) {
+        this.yRotation = degreeToRadian(angleInDegree);
+    }
+
+    /**
+     * Sets the rotation value for this shape on the y axis
+     * @param {number} angleInDegree degree to rotate the shape by on the y axis
+     */
+    setZRotation(angleInDegree) {
+        this.zRotation = degreeToRadian(angleInDegree);
     }
 }
